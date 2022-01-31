5bd0fe8c1d439a54dc36ba3701eccc7a0130a354
Valéria Garcia <valeriabeserragarcia@gmail.com>
2022-01-31 16:42:49 -0300

    exercicio 5 - metodo remover todos os elementos

--- src/com/valeria/estruturadados/vetor/Lista.java
+++ src/com/valeria/estruturadados/vetor/Lista.java
@@ -104,6 +104,20 @@ public class Lista<T> {
 			this.remove(pos);
 		}
 	}
+	
+	public void limpar() {
+		//opção 1
+		//this.elementos = (T[]) new Object[this.elementos.length];
+		
+		//opção 2
+		//this.tamanho = 0;
+		
+		//opção 3
+		for (int i=0; i<this.tamanho; i++){
+			this.elementos[i] = null;
+		}
+		this.tamanho = 0;
+	}
 
 	public int tamanho() {
 		return this.tamanho;
