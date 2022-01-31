0fac86056e478c546b0888d4b8e4e3a292425568
Valéria Garcia <valeriabeserragarcia@gmail.com>
2022-01-31 16:30:41 -0300

    exercicio 4 - metodo obtem

--- src/com/valeria/estruturadados/vetor/Lista.java
+++ src/com/valeria/estruturadados/vetor/Lista.java
@@ -50,8 +50,12 @@ public class Lista<T> {
 			this.elementos = elementosNovos;
 		}
 	}
+	
+	public T obtem(int posicao) {
+		return this.busca(posicao);
+	}
 
-	public Object busca(int posicao) {
+	public T busca(int posicao) {
 		if (!(posicao >= 0 && posicao < tamanho)) {
 			throw new IllegalArgumentException("posição inválida");
 		}
