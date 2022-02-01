8e18d14fd3b3c43bc367db7ade722708869f6a12
Valeria Garcia <valeriabeserragarcia@gmail.com>
2022-02-01 16:50:06 -0300

    Aula 13 - pilhas - introducao

---
+++ src/com/valeria/estruturadados/pilha/Pilha.java
@@ -0,0 +1,15 @@
+package com.valeria.estruturadados.pilha;
+
+import com.valeria.estruturadados.base.EstruturaEstatica;
+
+public class Pilha<T> extends EstruturaEstatica<T> {
+	
+	public Pilha() {
+		super();
+	}
+	
+	public Pilha(int capacidade) {
+		super(capacidade);
+	}
+
+}
---
+++ src/com/valeria/estruturadados/pilha/teste/Aula13.java
@@ -0,0 +1,15 @@
+package com.valeria.estruturadados.pilha.teste;
+
+import com.valeria.estruturadados.pilha.Pilha;
+
+public class Aula13 {
+
+	public static void main(String[] args) {
+		
+		Pilha<Integer> pilha = new Pilha<Integer>();
+		
+		System.out.println(pilha);
+		System.out.println(pilha.tamanho());
+	}
+
+}
