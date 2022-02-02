65968f2d901db770920aa6702ed95dcaca86696d
Valeria Garcia <valeriabeserragarcia@gmail.com>
2022-02-02 15:32:13 -0300

    Aula 20 - filas - enfileirando elementos

--- src/com/valeria/estruturadados/fila/Fila.java
+++ src/com/valeria/estruturadados/fila/Fila.java
@@ -11,5 +11,14 @@ public class Fila<T> extends EstruturaEstatica<T>{
 	public Fila	(int capacidade) {
 		super(capacidade);
 	}
+	
+	public void enfileira(T elemento) {
+		//this.elementos[this.tamanho] = elemento;
+		//this.tamanho++;
+		
+		//this.elementos[this.tamanho++] = elemento;
+		
+		this.adiciona(elemento);
+	}
 
 }
---
+++ src/com/valeria/estruturadados/fila/teste/Aula20.java
@@ -0,0 +1,22 @@
+package com.valeria.estruturadados.fila.teste;
+
+import com.valeria.estruturadados.fila.Fila;
+
+public class Aula20 {
+
+	public static void main(String[] args) {
+		
+		Fila<Integer> fila = new Fila<>();
+		
+		fila.enfileira(1);
+		fila.enfileira(2);
+		fila.enfileira(3);
+		
+		System.out.println(fila.estaVazia());
+		System.out.println(fila.tamanho());
+		
+		System.out.println(fila.toString());
+
+	}
+
+}
