f8bc98f06e0d8ad703f068a687ff7a5dfa19ffba
Valeria Garcia <valeriabeserragarcia@gmail.com>
2022-02-02 15:12:01 -0300

    Aula 19 - filas - introdução

---
+++ src/com/valeria/estruturadados/fila/Fila.java
@@ -0,0 +1,15 @@
+package com.valeria.estruturadados.fila;
+
+import com.valeria.estruturadados.base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T>{
+	
+	public Fila() {
+		super();
+	}
+	
+	public Fila	(int capacidade) {
+		super(capacidade);
+	}
+
+}
---
+++ src/com/valeria/estruturadados/fila/teste/Aula19.java
@@ -0,0 +1,16 @@
+package com.valeria.estruturadados.fila.teste;
+
+import com.valeria.estruturadados.fila.Fila;
+
+public class Aula19 {
+
+	public static void main(String[] args) {
+
+		Fila<Integer> fila = new Fila<>();
+		
+		System.out.println(fila.estaVazia());
+		System.out.println(fila.tamanho());
+
+	}
+
+}
